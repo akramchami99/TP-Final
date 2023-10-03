8fee163c0a33e86faebf8210e7af5fc10b5c9a7a
Anthony Boschat <138904122+AnthonyBoschat@users.noreply.github.com>
2023-10-03 23:03:05 +0200

    Update button_new_profil.js

--- button_new_profil.js
+++ button_new_profil.js
@@ -6,7 +6,7 @@ function main()
     listener_pilote()
 }
 
-/////////// Lancement des pilotes au chargement de la page
+//////////// Lancement des pilotes au chargement de la page
 function listener_pilote()
 {
     let button_new_profil = document.querySelector("#new_profil"),
