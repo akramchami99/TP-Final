fa9e829f033a2cb95a02d2b83b86acd34fa2cc7a
AnthonyBoschat <anthonyboschat.dev@hotmail.com>
2023-10-04 14:33:22 +0200

    feature pour le survol dans les profils

--- add_profil_name_in_nav.js
+++ add_profil_name_in_nav.js
@@ -1,4 +1,32 @@
 
+function focus_on_this_profil(event)
+{
+    let all_user_prenom = document.querySelectorAll(".user_prenom")
+    let prenom_under_mouse = event.target.innerHTML
+    all_user_prenom.forEach(prenom => 
+        {
+            if(prenom.innerHTML == prenom_under_mouse)
+            {
+                let profil_match = prenom.closest(".profil_design")
+                profil_match.classList.add("focus_on")
+            }
+        })
+}
+
+function remove_focus_on_this_profil(event)
+{
+    let all_user_prenom = document.querySelectorAll(".user_prenom")
+    let prenom_under_mouse = event.target.innerHTML
+    all_user_prenom.forEach(prenom => 
+        {
+            if(prenom.innerHTML == prenom_under_mouse)
+            {
+                let profil_match = prenom.closest(".profil_design")
+                profil_match.classList.remove("focus_on")
+            }
+        })
+}
+
 function load_all_name_in_nav_bar()
 {
     // On selectionne la destination
@@ -25,7 +53,16 @@ function load_all_name_in_nav_bar()
     }
 
     // Une fois que les noms de profils ont été injecter, on ajoute un listener pour le survol de la souris sur les noms
-    
+    let all_profils_names = destination.querySelectorAll(".profil-container")
+    // On ajoute un listener au survol
+    all_profils_names.forEach(profil => 
+        {
+            profil.removeEventListener("mouseover", focus_on_this_profil)
+            profil.addEventListener("mouseover", focus_on_this_profil)
+
+            profil.removeEventListener("mouseout", remove_focus_on_this_profil)
+            profil.addEventListener("mouseout", remove_focus_on_this_profil)
+        })
 }
 
 load_all_name_in_nav_bar()
